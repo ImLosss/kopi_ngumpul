42e0ec405dc45c5492c9c648cb0c4b6736ecc836
ImLosss <pahriansyahryan@gmail.com>
2024-06-24 13:57:43 +0800

    update logic

--- resources/views/admin/pesanan/show.blade.php
+++ resources/views/admin/pesanan/show.blade.php
@@ -119,9 +119,17 @@
                 $('#btnUpdate').append(code);
 
                 var anyChecked = $('.selectPesan[data-payment="true"]:checked').length > 0;
+                var anyCantDelete = $('.check-delete[data-hapus="false"]').length > 0;
                 
                 // console.log(anyChecked);
                 if (anyChecked) {
+                    $('#btnHapus').attr('disabled', 'disabled');
+                } else {
+                    $('#btnHapus').removeAttr('disabled');
+                    // $('#btnHapus').attr('disabled', 'disabled');
+                }
+
+                if (anyCantDelete) {
                     $('#btnHapus').attr('disabled', 'disabled');
                 } else {
                     $('#btnHapus').removeAttr('disabled');
