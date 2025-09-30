bf6e26e9b6092a13a52be4ac2f14e4795e92ae07
ImLosss <pahriansyahryan@gmail.com>
2025-09-30 09:56:46 +0800

    update seeder

--- app/Http/Controllers/admin/AdminController.php
+++ app/Http/Controllers/admin/AdminController.php
@@ -3,17 +3,6 @@
 namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
-use App\Models\Cart;
-use App\Models\Category;
-use App\Models\IngredientTransaction;
-use App\Models\Order;
-use App\Models\Product;
-use App\Models\Stock;
-use App\Models\User;
-use Carbon\Carbon;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-use Yajra\DataTables\Facades\DataTables;
 
 class AdminController extends Controller
 {
